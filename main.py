ed603559b8ecb91d6781acfa292b8d7fc4d86d43
GabeGibb <97437160+GabeGibb@users.noreply.github.com>
2023-01-18 16:19:25 -0800

    PLEASE

--- main.py
+++ main.py
@@ -18,16 +18,16 @@ app = App()
 bot = commands.Bot(command_prefix='*', intents=discord.Intents.all())
 updater = Updater(bot, app)
 
-lolPlayers = pd.read_csv('players/lolPlayers.csv')['lol']
-for p in lolPlayers:
-    print(p)
-    app.add_lol_acct(p)
+# lolPlayers = pd.read_csv('players/lolPlayers.csv')['lol']
+# for p in lolPlayers:
+#     print(p)
+#     app.add_lol_acct(p)
 
-valPlayers = pd.read_csv('players/valPlayers.csv')['val']
-for p in valPlayers:
-    user, tag = p.split('#')
-    print(user, tag)
-    app.add_val_acct(user, tag)
+# valPlayers = pd.read_csv('players/valPlayers.csv')['val']
+# for p in valPlayers:
+#     user, tag = p.split('#')
+#     print(user, tag)
+#     app.add_val_acct(user, tag)
 
 @bot.event
 async def on_ready():
@@ -37,6 +37,7 @@ async def on_ready():
     updater.channel = channel
     updater.lol_update.start()
     updater.val_update.start()
+    await updater.channel.send('Connected!')
 
 
 @bot.command()
