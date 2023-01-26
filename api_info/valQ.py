a601ef8e9c141dd0ad99b5e24f69a390cfb70a31
GabeGibb <97437160+GabeGibb@users.noreply.github.com>
2023-01-26 01:50:43 -0800

    new key and implemented val key

--- api_info/valQ.py
+++ api_info/valQ.py
@@ -1,12 +1,16 @@
 import requests
 
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('VAL_KEY')
 
 def get_val_info(username, tagline):
-    response = requests.get(f"https://api.henrikdev.xyz/valorant/v1/mmr/na/{username}/{tagline}")
+    headers = {'Authorization': TOKEN}
+    response = requests.get(f"https://api.henrikdev.xyz/valorant/v1/mmr/na/{username}/{tagline}", headers=headers)
     data = response.json()['data']
     return data
 
 
-
-print(get_val_info('LAVISHMONKEY56', 'NA1'))
 # print(get_val_info('kachatakoowl', 'NA1'))
